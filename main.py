84d1eb288870f562c33c31e04132f864a84d77c4
S0HYUNN <pshsh0516@naver.com>
2023-08-08 14:30:50 +0900

    :memo: google_login

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-from flask import Flask, render_template, url_for, redirect, session
+
+from flask import Flask, render_template, request, send_file, session, redirect
 import os
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from forms import InForm
+from oauth2client.contrib.flask_util import UserOAuth2
+from werkzeug.utils import secure_filename
+from sqlalchemy import MetaData, Table, Column, String
+from dbconn import get_con
+
 app = Flask(__name__)
 ###########################
 ###### DB setup ###########
 ###########################
 app.config['SECRET_KEY'] = 'mysecretkey'
+
+app.config['GOOGLE_OAUTH2_CLIENT_SECRETS_FILE'] = 'secret.json'
+#app.config['GOOGLE_OAUTH2_CLIENT_ID'] = os.environ['OAUTH_CLIENT']
+#app.config['GOOGLE_OAUTH2_CLIENT_SECRET'] = os.environ['OAUTH_SECRET']
+
+oauth2 = UserOAuth2(app)
 
 ######## DB Section ##########
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -15,6 +27,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 input_db=SQLAlchemy(app)
 Migrate(app, input_db)
+
+# GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", '516475944042-psbar5tjohs8qqdfcjibejuhv9nnscpe.apps.googleusercontent.com')
+# GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", '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')
+
 
 ######## MODELS ##############
 class Input(input_db.Model):
@@ -34,7 +50,18 @@ class Input(input_db.Model):
     def __repr__(self):
         return f"{self.type}: {self.price}won"
 
-
+class login(input_db.Model):
+    __tablename__ = 'login'
+    email = input_db.Column(input_db.Text, primary_key = True)
+    name = input_db.Column(input_db.Text)
+    
+    def __init__(self, email, name):
+        self.email = email
+        self.name = name
+        
+    def __repr__(self):
+        return f"email : {self.email} name : {self.name}"
+    
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = InForm()
@@ -49,6 +76,36 @@ def index():
         input_db.session.commit()
         return redirect(url_for('list'))
     return render_template('index.html', form=form)
+
+@app.route('/login')
+def login():
+    test_db = Input.query.all()
+    return render_template('login.html', test_db = test_db)
+
+@app.route('/oauth2authorize')
+@oauth2.required
+def google_oauth():
+    print("Google OAuth>> {} ({})".format(oauth2.email, oauth2.user_id))
+    print("email : ", oauth2.email)
+    print("name : ", oauth2.name)
+    return redirect('/')
+    # u = User.query.filter('email = : email').params(email=oauth2.email).first()
+    # if u is not None:
+    #     session['loginUser'] = {'userid' : u.id, 'name' : u.nickname}
+    #     return redirect('/')
+    
+    # else:
+    #     flask("해당 사용자가 없습니다!!")
+    #     return render_template("login.html", email = oauth2.email)    
+
+@app.route('/logout')
+def logout():
+    if session.get('loginUser'):
+        del session['loginUser']
+        session.modified = True
+        oauth2.storage.delete()
+        
+    return redirect('/')
 
 @app.route('/list')
 def list():
