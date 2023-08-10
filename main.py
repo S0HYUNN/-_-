a0c89eb31895082b5884f8e69a1ecae283898e88
S0HYUNN <pshsh0516@naver.com>
2023-08-10 22:15:12 +0900

    :sparkles: signup

--- main.py
+++ main.py
@@ -162,5 +162,9 @@ def projects():
 def resume():
     return render_template('resume.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 if __name__ == '__main__':
     app.run(port="5000", debug = True)
