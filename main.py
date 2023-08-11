71df1d13db57965ca48a153e7fb7a6db7c9fe0d9
Jihyeoniiiii <105184159+Jihyeoniiiii@users.noreply.github.com>
2023-08-11 14:47:28 +0900

    :sparkles: Add features

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('testimonial.html')
+    return render_template('output.html')
 
 if __name__ == '__main__':
     app.run(port="5000", debug = True)
---
+++ static/js/chart.js
@@ -0,0 +1,98 @@
+$(function() {
+  /* ChartJS
+   * -------
+   * Data and config for chartjs
+   */
+ 
+  var doughnutPieData = {
+    datasets: [{
+      data: [40, 30, 20],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.5)',
+        'rgba(54, 162, 235, 0.5)',
+        'rgba(255, 206, 86, 0.5)',
+        'rgba(75, 192, 192, 0.5)',
+        'rgba(153, 102, 255, 0.5)',
+        'rgba(255, 159, 64, 0.5)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+      '# Pink',
+      '# Blue',
+      '# Yellow',
+    ]
+  };
+  var doughnutPieOptions = {
+    responsive: true,
+    animation: {
+      animateScale: true,
+      animateRotate: true
+    }
+    , responsive: false
+  };
+  var areaData = {
+    labels: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"
+    , "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"
+    , "21", "22", "23", "24"],
+    datasets: [{
+      label: 'Number of posts',
+      data: [12, 19, 3, 5, 2, 3, 6, 2, 1, 10, 15, 
+        12, 19, 3, 5, 2, 3, 6, 2, 1, 10, 15, 3, 5, 2, 1],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1,
+      fill: true, // 3: no fill
+    }]
+  };
+
+  var areaOptions = {
+    plugins: {
+      filler: {
+        propagate: true
+      }
+    }
+    , responsive: false
+  }
+
+  if ($("#pieChart").length) {
+    var pieChartCanvas = $("#pieChart").get(0).getContext("2d");
+    var pieChart = new Chart(pieChartCanvas, {
+      type: 'pie',
+      data: doughnutPieData,
+      options: doughnutPieOptions
+    });
+  }
+
+  if ($("#areaChart").length) {
+    var areaChartCanvas = $("#areaChart").get(0).getContext("2d");
+    var areaChart = new Chart(areaChartCanvas, {
+      type: 'line',
+      data: areaData,
+      options: areaOptions
+    });
+  }
+});
