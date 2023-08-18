8fadd274c2009d7d2e84556e1196f55ce198ee18
Jihyeoniiiii <105184159+Jihyeoniiiii@users.noreply.github.com>
2023-08-18 15:42:53 +0900

    :memo: form

--- forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField, RadioField, TextAreaField, SelectField
-
-class InForm(FlaskForm):
-    user_id = StringField("User ID: ")
-    type = SelectField("Type of Clothing: ", choices=[('bag','Bag'), ('shirt', 'Shirt'), ('jeans', 'Jeans')])
-    tags = TextAreaField(u"Preferred Hashtags: ")
-    style = RadioField("Choose Style: ", choices=[('Sungsu', 'Sungsu'), ('Hongdae', 'Hongdae'), ('Gangnam','Gangnam'), ('Sinsa', 'Sinsa')])
-    submit = SubmitField()
