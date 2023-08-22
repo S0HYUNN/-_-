ea1413de22c7e658c8fcc99ab534f62f59da1a7f
Jihyeoniiiii <105184159+Jihyeoniiiii@users.noreply.github.com>
2023-08-22 20:39:53 +0900

    :art: output

--- main.py
+++ main.py
@@ -124,8 +124,9 @@ def callback():
 @app.route('/signup', methods=['GET','POST'])
 def signup():
     #     # 임시
-    # session["email"]="jiwoongmun@gmail.com"
-    # session["name"] = "Jiwoong"
+    session["email"]="jiwoongmun@gmail.com"
+    session["name"] = "Jiwoong"
+    session["username"] = "jihyeoni"
     ## 회원 가입 절차 시작 ##
     form = Login()
     if form.validate_on_submit():
