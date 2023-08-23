f33e7f10a6469b8a4b09776c14f3c1fd6c3f1a78
jiwoong <jiwoongmun@gmail.com>
2023-08-23 15:30:20 +0900

    output graph db access

--- main.py
+++ main.py
@@ -228,9 +228,20 @@ def projects_output():
     Pfocus = request.args.get("Pfocus")
     return render_template('output_copy.html', name=session['name'], username=session.get('username'), type=Ptype ,season=Pseason, style=Pstyle, focus=Pfocus)
 
-@app.route('/output')
+@app.route('/output', methods=['GET', 'POST'])
 def output():
-    return render_template('output_copy.html', name=session['name'], username=session.get('username'), type=session['type'] ,season=session['season'], style=session['style'], focus=session['focus'])
+    # inf_name = json.loads(request.data)
+    sql1= f"select year_month, comments_count_mean, like_count_mean from full_date where username='jiseong'"
+    sql2 = f"select media_product_type, media_type, comments_count_mean, like_count_mean from full_type where username='jiseong'"
+    conn = get_con()
+    sql_result1 = pd.read_sql(sql1, conn)
+    sql_result2 = pd.read_sql(sql2, conn)
+    result1 = sql_result1.to_dict('records')
+    result2 = sql_result2.to_dict('records')
+
+    graph_data = json.dumps({'result1':result1,'result2':result2}, ensure_ascii=False)
+    print(graph_data)
+    return render_template('output_copy.html', name=session['name'], username=session.get('username'), type=session['type'] ,season=session['season'], style=session['style'], focus=session['focus'], data= graph_data)
 
 
 @app.route('/base')
